b53b0522204a3b6ae5a428584b51d69f7ea8d982
remusp <remus8802@yahoo.com>
2019-06-12 12:04:02 +0300

    Update AssemblyInfo.cs

--- MultiSelectComboBox/MultiSelectComboBox/Properties/AssemblyInfo.cs
+++ MultiSelectComboBox/MultiSelectComboBox/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 
 [assembly: XmlnsPrefix("http://schemas.sdl.com/xaml", "sdl")]
 [assembly: XmlnsDefinition("http://schemas.sdl.com/xaml", "Sdl.MultiSelectComboBox.Themes.Generic")]
