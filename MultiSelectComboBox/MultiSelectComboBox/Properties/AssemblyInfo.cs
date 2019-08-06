7ad0ad8342df903376b9ab8cf4538b4c8522509c
skerestes <53822262+skerestes@users.noreply.github.com>
2019-08-06 15:31:37 +0300

    fixed missing parenthesis

--- MultiSelectComboBox/MultiSelectComboBox/Properties/AssemblyInfo.cs
+++ MultiSelectComboBox/MultiSelectComboBox/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8")
+[assembly: AssemblyVersion("1.0.8")]
 [assembly: AssemblyFileVersion("1.0.8.0")]
 
 [assembly: XmlnsPrefix("http://schemas.sdl.com/xaml", "sdl")]
