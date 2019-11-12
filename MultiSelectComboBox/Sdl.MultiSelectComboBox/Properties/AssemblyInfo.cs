340d99903fb81edabade2c4767bf658a05ab07e3
ai191 <ai191@users.noreply.github.com>
2019-11-12 14:05:37 -0800

    Updates the version number to 1.0.16

--- MultiSelectComboBox/Sdl.MultiSelectComboBox/Properties/AssemblyInfo.cs
+++ MultiSelectComboBox/Sdl.MultiSelectComboBox/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows.Markup;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sdl.MultiSelectComboBox")]
-[assembly: AssemblyCopyright("Copyright © SDL 2018")]
+[assembly: AssemblyCopyright("Copyright © SDL 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,8 +50,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.15")]
-[assembly: AssemblyFileVersion("1.0.15.0")]
+[assembly: AssemblyVersion("1.0.16")]
+[assembly: AssemblyFileVersion("1.0.16.0")]
 
 [assembly: XmlnsPrefix("http://schemas.sdl.com/xaml", "sdl")]
 [assembly: XmlnsDefinition("http://schemas.sdl.com/xaml", "Sdl.MultiSelectComboBox.Themes.Generic")]
