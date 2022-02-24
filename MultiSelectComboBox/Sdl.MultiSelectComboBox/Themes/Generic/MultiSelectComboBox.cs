39b22605935e08539c005c1c934486acfb2ead4a
Mitch Capper <mitch.capper@gmail.com>
2022-02-24 15:43:22 -0800

    Prevent stealing focus back when clicking an outside control
        Closes #60

--- MultiSelectComboBox/Sdl.MultiSelectComboBox/Themes/Generic/MultiSelectComboBox.cs
+++ MultiSelectComboBox/Sdl.MultiSelectComboBox/Themes/Generic/MultiSelectComboBox.cs
@@ -1,4 +1,4 @@
-﻿using Sdl.MultiSelectComboBox.API;
+using Sdl.MultiSelectComboBox.API;
 using Sdl.MultiSelectComboBox.Controls;
 using Sdl.MultiSelectComboBox.EventArgs;
 using Sdl.MultiSelectComboBox.Services;
@@ -368,11 +368,14 @@ namespace Sdl.MultiSelectComboBox.Themes.Generic
 			MultiSelectComboBox comboBox = sender as MultiSelectComboBox;
 			if (comboBox != null)
 			{
+				if (comboBox.IsDropDownOpen)
+					comboBox.IgnoreDropdownClosingFocusPlanUntil = DateTime.Now.AddSeconds(1);
 				comboBox.CloseDropdownMenu(comboBox.ClearFilterOnDropdownClosing, false);
 				comboBox.CaptureMouse();
 				comboBox.ReleaseMouseCapture();
 			}
 		}
+		private DateTime? IgnoreDropdownClosingFocusPlanUntil;
 
 		public override void OnApplyTemplate()
 		{
@@ -1234,6 +1237,11 @@ namespace Sdl.MultiSelectComboBox.Themes.Generic
 
 		private void DropdownMenuClosed(object sender, System.EventArgs e)
 		{
+			if (IgnoreDropdownClosingFocusPlanUntil > DateTime.Now)
+			{
+				IgnoreDropdownClosingFocusPlanUntil = null;//so only the first is ignored
+				return;
+			}
 			FocusCursorOnFilterTextBox();
 		}
 
