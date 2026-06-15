7bdfb3add48167888eb25c7e70d34a74762e2d41
BogdanCostisorRWS <bcostisor@rws.com>
2026-06-15 15:30:00 +0300

    LG-53151 [Themes] [New Project] Add support for Multi Language Select Control (#115)
    Co-authored-by: Bogdan Costisor <bcostisor@sdl.com>

---
+++ MultiSelectComboBox/Sdl.MultiSelectComboBox/Themes/ThemeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sdl.MultiSelectComboBox.Themes
+{
+    public static class ThemeManager
+    {
+        private static readonly Uri DefaultThemeUri = new Uri(
+            "pack://application:,,,/Sdl.MultiSelectComboBox;component/Themes/Default.xaml",
+            UriKind.Absolute);
+
+        private static readonly Uri HighContrastThemeUri = new Uri(
+            "pack://application:,,,/Sdl.MultiSelectComboBox;component/Themes/HighContrast.xaml",
+            UriKind.Absolute);
+
+        private static ResourceDictionary _controlResources;
+
+
+        public static void Startup(ResourceDictionary controlResources)
+        {
+            _controlResources = controlResources;
+
+            ApplyTheme();
+
+            SystemParameters.StaticPropertyChanged += (_, e) =>
+            {
+                if (e.PropertyName == nameof(SystemParameters.HighContrast))
+                {
+                    ApplyTheme();
+                }
+            };
+        }
+
+        private static void ApplyTheme()
+        {
+            var targetUri = SystemParameters.HighContrast ? HighContrastThemeUri : DefaultThemeUri;
+            var merged = _controlResources.MergedDictionaries;
+
+            for (int i = merged.Count - 1; i >= 0; i--)
+            {
+                var src = merged[i].Source;
+                if (src == DefaultThemeUri || src == HighContrastThemeUri)
+                {
+                    merged.RemoveAt(i);
+                }
+            }
+
+            merged.Insert(0, new ResourceDictionary { Source = targetUri });
+        }
+    }
+}
